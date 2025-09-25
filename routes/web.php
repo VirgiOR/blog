aff473799441326fa76004dd1019fe4c4148a8dd
virginia Ortiz <virginia.centroseneca@gmail.com>
2025-09-25 12:02:43 +0200

    Creamos controladoresy vistas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\PostController;
+
+
+
+Route::get('/', HomeController::class);
+
+Route::get('/posts', [PostController::class,'index']);
+
+Route::get('/posts/create', [PostController::class,'create']);
+
+Route::get('/posts/{post}', [PostController::class,'show']);
+
+
+/*Route::get('/posts/{post}/{category}', function( $post,$category) {
+    return "Aquí se mostrará el post $post de la categoría $category";
+});*/
+
+
+
+
+
